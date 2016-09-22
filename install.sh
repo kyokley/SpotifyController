148509e23dca122e10a4854abdffe9b0d967a60f
Kevin Yokley <kyokley2@gmail.com>
2016-09-22 13:30:05 -0500

    Adding sp script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo aptitude install xdotool
+sudo ln -s ~/workspace/spotify/sp /usr/local/bin/
--- spotify.sh
+++ spotify.sh
@@ -4,10 +4,15 @@
 #If this belongs to you, let me know so I can give the proper props!
 
 # Get spotify window id, this works only if spotify is runnin and playing
-id=$(xdotool search --name "Spotify Premium")
-[ $? -ne 0 ] && echo "Failed" && exit 1
+#id=$(xdotool search --name "Spotify Premium")
+#[ $? -ne 0 ] && echo "Failed" && exit 1
 
 # Send keystrokes to control spotify
-[ "$1" = "n" ] && xdotool key --window $id Control_L+Right
-[ "$1" = "p" ] && xdotool key --window $id Control_L+Left
-[ "$1" = "pause" ] && xdotool key --window $id space
+#[ "$1" = "n" ] && xdotool key --window $id Control_L+Right
+#[ "$1" = "p" ] && xdotool key --window $id Control_L+Left
+#[ "$1" = "pause" ] && xdotool key --window $id space
+
+# Switch to using sp script
+[ "$1" = "n" ] && sp next
+[ "$1" = "p" ] && sp prev
+[ "$1" = "pause" ] && sp play
