bb20d3e4d43a2b0f480adf5b95fcd1ba80eb9218
Kevin Yokley <kyokley2@gmail.com>
2020-07-29 14:17:46 -0500

    Add manjaro install steps

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-sudo aptitude install xdotool
+USE_PAMAC=$(which apt-get >/dev/null 2>&1 && echo "true")
+USE_APT_GET=$(which apt-get >/dev/null 2>&1 && echo "true")
+
+if [ -n $USE_APT_GET ]
+then
+    sudo aptitude install xdotool
+fi
+
+if [ -n $USE_PAMAC ]
+then
+    pamac install xdotool
+fi
+
 sudo ln -s $(pwd)/sp /usr/local/bin/
